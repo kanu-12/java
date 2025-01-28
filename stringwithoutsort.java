6d9dc46b57af61702f42c569ca4a94437930ef3c
Siddharth Negi <148892322+kanu-12@users.noreply.github.com>
2025-01-28 21:14:16 +0530

    Add files via upload

---
+++ stringwithoutsort.java
@@ -0,0 +1,33 @@
+public class stringwithoutsort {
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] count = new int[26];
+
+        
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int c : count) {
+            if (c != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "anagram";
+        String t1 = "nagaram";
+        System.out.println(isAnagram(s1, t1)); 
+
+        String s2 = "rat";
+        String t2 = "car";
+        System.out.println(isAnagram(s2, t2)); 
+    }
+}
